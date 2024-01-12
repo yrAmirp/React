3b57ed01f51d4ee03b33dd51cc63835e519f30d8
Amir <khalamir.amir@gmail.com>
2024-01-12 19:18:13 +0300

    added react-router-dom, routing, lazy page loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { Counter } from './components/Counter';
+import { Link, Route, Routes } from 'react-router-dom';
 import './index.scss';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { Suspense } from 'react';
+
 const App = () => {
   return (
     <div className='app'>
-      HELLO!
-      <Counter />
+      <Link to={'/'}> Главная </Link>
+      <Link to={'/about'}> О сайте </Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageAsync />} />
+          <Route path={'/'} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
